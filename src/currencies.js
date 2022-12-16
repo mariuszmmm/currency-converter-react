9d3fd6f8fe888eedc3c4327bd9e333d784210278
Mariusz Matusiewicz <mariuszmmm@op.pl>
2022-12-16 22:37:31 +0100

    added curencies

--- src/currencies.js
+++ src/currencies.js
@@ -2,26 +2,38 @@
 export const currencies = [
   {
     symbol: "PLN",
-    name: "Złoty",
+    name: "złoty",
     rate: 1,
     unit: "zł",
   },
   {
-    symbol: "EUR",
-    name: "Euro",
-    rate: 4.6843,
-    unit: "€",
-  },
-  {
     symbol: "USD",
-    name: "Dolar",
-    rate: 4.4100,
+    name: "dolar",
+    rate: 4.4227,
     unit: "$",
   },
   {
+    symbol: "EUR",
+    name: "euro",
+    rate: 4.6934,
+    unit: "€",
+  },
+   {
     symbol: "GBP",
-    name: "Funt",
-    rate: 5.4418,
+    name: "funt",
+    rate: 5.3716,
     unit: "£",
+  },
+  {
+    symbol: "CNY",
+    name: "yuan",
+    rate: 0.6341,
+    unit: "¥",
+  },
+  {
+    symbol: "UAH",
+    name: "hrywna",
+    rate: 0.1258,
+    unit: "грн",
   },
 ];
