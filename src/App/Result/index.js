01e599062c8b218d3b73b1eee308d5738258a318
Mariusz Matusiewicz <mariuszmmm@op.pl>
2022-12-17 17:39:30 +0100

    changed spaces

--- src/App/Result/index.js
+++ src/App/Result/index.js
@@ -2,13 +2,13 @@ import "./style.css";
 
 const Result = ({ result, resultOutdated }) => (
 
-  <div className={`form__result ${resultOutdated ? "form__result--invalid" : ""}`}>
-    {result !== undefined && (
-      <>
-        {result.exchangeResult.toFixed(2)}&nbsp;{result.unit}
-      </>
-    )}
-  </div>
+   <div className={`form__result ${resultOutdated ? "form__result--invalid" : ""}`}>
+      {result !== undefined && (
+         <>
+            {result.exchangeResult.toFixed(2)}&nbsp;{result.unit}
+         </>
+      )}
+   </div>
 );
 
 export default Result;
--- src/App/currencies.js
+++ src/App/currencies.js
@@ -1,39 +1,39 @@
 
 export const currencies = [
-  {
-    symbol: "PLN",
-    name: "złoty",
-    rate: 1,
-    unit: "zł",
-  },
-  {
-    symbol: "USD",
-    name: "dolar",
-    rate: 4.4227,
-    unit: "$",
-  },
-  {
-    symbol: "EUR",
-    name: "euro",
-    rate: 4.6934,
-    unit: "€",
-  },
-  {
-    symbol: "GBP",
-    name: "funt",
-    rate: 5.3716,
-    unit: "£",
-  },
-  {
-    symbol: "CNY",
-    name: "yuan",
-    rate: 0.6341,
-    unit: "¥",
-  },
-  {
-    symbol: "UAH",
-    name: "hrywna",
-    rate: 0.1258,
-    unit: "грн",
-  },
+   {
+      symbol: "PLN",
+      name: "złoty",
+      rate: 1,
+      unit: "zł",
+   },
+   {
+      symbol: "USD",
+      name: "dolar",
+      rate: 4.4227,
+      unit: "$",
+   },
+   {
+      symbol: "EUR",
+      name: "euro",
+      rate: 4.6934,
+      unit: "€",
+   },
+   {
+      symbol: "GBP",
+      name: "funt",
+      rate: 5.3716,
+      unit: "£",
+   },
+   {
+      symbol: "CNY",
+      name: "yuan",
+      rate: 0.6341,
+      unit: "¥",
+   },
+   {
+      symbol: "UAH",
+      name: "hrywna",
+      rate: 0.1258,
+      unit: "грн",
+   },
 ];
