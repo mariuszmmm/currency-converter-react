6ebd4183e5e4dee68a0bd15c20709436cf23df5d
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-01-04 21:27:48 +0100

    added filter to Rates

--- src/App/Rates/index.js
+++ src/App/Rates/index.js
@@ -1,26 +1,24 @@
 import Section from "../Section";
 import { currencies } from "../currencies";
 
-const Rates = () => {
-   return (
-      currencies.map((currency) => {
-         if (currency.symbol !== "PLN") {
-            return (
-               <Section key={currency.symbol}
-                  title={`Kurs ${currency.symbol} :`}
-                  body={
-                     <input
-                        name="rate"
-                        defaultValue={currency.rate}
-                        className="form__field js-rateEUR"
-                        disabled
-                     />
-                  }
+const currenciesWithOutPLN = currencies.filter(({ symbol }) => symbol !== "PLN");
+
+const Rates = () => (
+   currenciesWithOutPLN.map((currency) => {
+      return (
+         <Section key={currency.symbol}
+            title={`Kurs ${currency.symbol} :`}
+            body={
+               <input
+                  name="rate"
+                  defaultValue={currency.rate}
+                  className="form__field js-rateEUR"
+                  disabled
                />
-            )
-         }
-      })
-   )
-};
+            }
+         />
+      )
+   })
+);
 
 export default Rates;
