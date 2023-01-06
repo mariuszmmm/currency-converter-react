eeb898e34b9c444fe1344f337522019f713c9c6d
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-01-06 16:47:52 +0100

    added toLocaleString to Result

--- src/App/Result/index.js
+++ src/App/Result/index.js
@@ -1,11 +1,10 @@
 import "./style.css";
 
 const Result = ({ result, resultOutdated }) => (
-
    <div className={`form__result ${resultOutdated ? "form__result--invalid" : ""}`}>
       {result !== undefined && (
          <>
-            {result.exchangeResult.toFixed(2)}
+            {Number((result.exchangeResult).toFixed(2)).toLocaleString()}
             &nbsp;
             {result.unit}
          </>
