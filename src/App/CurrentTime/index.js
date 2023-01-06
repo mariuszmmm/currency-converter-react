0b93436cc84db7ec7188684bd817d603a070bcec
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-01-06 16:10:36 +0100

    added &nbsp; to currentTime

--- src/App/CurrentTime/index.js
+++ src/App/CurrentTime/index.js
@@ -1,7 +1,13 @@
 import "./style.css";
 
 const CurrentTime = ({ currentTime }) => (
-	<p className="form__date">Dzisiaj jest {currentTime.toLocaleString("pl", { weekday: "long", day: "numeric", month: "long" })}, {currentTime.toLocaleTimeString()}</p>
+	<p className="form__date">
+		Dzisiaj jest&nbsp;
+		{currentTime.toLocaleString(
+			"pl", { weekday: "long", day: "numeric", month: "long" }
+		)},
+		{currentTime.toLocaleTimeString()}
+	</p>
 );
 
 export default CurrentTime;
