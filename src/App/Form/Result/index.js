f77f49bb8e6de1f28dfbfe39dd9b730884bdbd65
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-11-20 16:21:50 +0100

    changed component Result

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -3,25 +3,20 @@ import { Container, AmountInfo, ResultInfo } from "./styled"
 const resultText = (number) => `${Number((number).toFixed(2)).toLocaleString("pl-PL", { minimumFractionDigits: 2 })}`;
 
 export const Result = ({ result, resultOutdated }) => (
-
-   <Container outdated={resultOutdated}>
-      <AmountInfo>
-         {result && (
-            <>
-               {resultText(Number(result.amount))}
-               &nbsp;&nbsp;
-               {result.currencyInputSign}&nbsp;=
-            </>
-         )}
-      </AmountInfo>
-      <ResultInfo>
-         {result && (
-            <>
-               {resultText(result.exchangeResult)}
-               &nbsp;&nbsp;
-               {result.currencyOutputSign}
-            </>
-         )}
-      </ResultInfo>
-   </Container >
+  <Container outdated={resultOutdated}>
+    {result &&
+      <>
+        <AmountInfo>
+          {resultText(Number(result.amount))}
+          &nbsp;&nbsp;
+          {result.currencyInputSign}&nbsp;=
+        </AmountInfo>
+        <ResultInfo>
+          {resultText(result.exchangeResult)}
+          &nbsp;&nbsp;
+          {result.currencyOutputSign}
+        </ResultInfo>
+      </>
+    }
+  </Container >
 );
