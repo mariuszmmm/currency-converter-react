f922cc5b8f406f6ea2ec28218c45d57e57931a91
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-11-20 16:22:27 +0100

    changed component Rates

--- src/App/Form/Rates/index.js
+++ src/App/Form/Rates/index.js
@@ -3,23 +3,24 @@ import { Input } from "../styled";
 import { Flags } from "../Flags";
 
 export const Rates = ({ data, currencyOutput }) => {
-   const { PLN, ...currenciesData } = data.currenciesData;
-   const sortedCurrenciesData = Object.values(currenciesData).sort((a, b) => a.code.localeCompare(b.code))
+  const { PLN, ...currenciesData } = data.currenciesData;
+  const sortedCurrenciesData = Object.values(currenciesData).sort((a, b) => a.code.localeCompare(b.code));
+  const displayStyle = (prop) => prop.toFixed(4).toString().replace('.', ',');
 
-   return (
-      sortedCurrenciesData.map((object) => (
-         <Section key={object.code}
-            title={currencyOutput === "PLN" ? `${object.code} / PLN :` : `PLN / ${object.code} :`}
-            body={
-               <Flags flag={object.flag}>
-                  <Input rate="true"
-                     name="rate"
-                     value={currencyOutput === "PLN" ? ((1 / object.rate).toFixed(4)).toString().replace('.', ',') : (object.rate.toFixed(4)).toString().replace('.', ',')}
-                     disabled
-                  />
-               </Flags>
-            }
-         />
-      ))
-   )
+  return (
+    sortedCurrenciesData.map((object) => (
+      <Section key={object.code}
+        title={currencyOutput === "PLN" ? `${object.code} / PLN :` : `PLN / ${object.code} :`}
+        body={
+          <Flags flag={object.flag}>
+            <Input rate="true"
+              name="rate"
+              value={currencyOutput === "PLN" ? displayStyle(1 / object.rate) : displayStyle(object.rate)}
+              disabled
+            />
+          </Flags>
+        }
+      />
+    ))
+  )
 };
