8b21fd8e10e4aa109c11bb7fbb44c18430498b73
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-11-20 16:23:51 +0100

    change theme

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,15 @@
 export const theme = {
-   color: {
-      white: "#ffffff",
-      silver: "#bbbbbb",
-      grey: "#808080",
-      doveGray: "#666666",
-      prussianBlue: "#003657",
-      red: "#FF0000",
-      black: "#000000",
-   },
-   breakpoint: {
-      mobileMin: 453,
-   }
+  color: {
+    white: "#ffffff",
+    silver: "#bbbbbb",
+    grey: "#808080",
+    doveGray: "#666666",
+    prussianBlue: "#003657",
+    red: "#FF0000",
+    black: "#000000",
+  },
+  breakpoint: {
+    mobileMin: 457,
+    mobileMax: 720
+  }
 };
