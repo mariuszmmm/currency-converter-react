7f3157589989531d5870080a17f87b74f6c6032d
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-11-20 16:23:03 +0100

    changed comopnent Option

--- src/App/Form/Options/index.js
+++ src/App/Form/Options/index.js
@@ -1,22 +1,24 @@
-export const Options = ({ data }) => {
-   const dataSortedByCurrency = Object.values(data.currenciesData).sort((a, b) => a.currency.localeCompare(b.currency))
-   const dataSortedByCode = Object.values(data.currenciesData).sort((a, b) => a.code.localeCompare(b.code))
-   const windowFull = window.innerWidth > 700 ? true : false;
-   const currencies = windowFull ? dataSortedByCurrency : dataSortedByCode
+import { theme } from "../../../theme";
 
-   return (
-      currencies.map((currency) =>
-         <option
-            key={currency.code}
-            value={currency.code}
-         >
-            {windowFull && currency.currency
-               ?
-               `${currency.currency} / ${currency.code}`
-               :
-               `${currency.code}`
-            }
-         </option>
-      )
-   )
+export const Options = ({ data }) => {
+  const dataSortedByCurrency = Object.values(data.currenciesData).sort((a, b) => a.currency.localeCompare(b.currency));
+  const dataSortedByCode = Object.values(data.currenciesData).sort((a, b) => a.code.localeCompare(b.code));
+  const windowMobileMax = (window.innerWidth > theme.breakpoint.mobileMax ? true : false);
+  const currencies = windowMobileMax ? dataSortedByCurrency : dataSortedByCode;
+
+  return (
+    currencies.map((currency) =>
+      <option
+        key={currency.code}
+        value={currency.code}
+      >
+        {windowMobileMax && currency.currency
+          ?
+          `${currency.currency} / ${currency.code}`
+          :
+          `${currency.code}`
+        }
+      </option>
+    )
+  )
 };
