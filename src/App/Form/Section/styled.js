525c926f06ecc2354d65a09fe181d0be4268589b
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-11-20 16:23:34 +0100

    changed style

--- src/App/Form/Section/styled.js
+++ src/App/Form/Section/styled.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const StyledSection = styled.section`
-   margin: 0 5px;
+  margin: 0 5px;
 `;
 
 export const Content = styled.span`
-   display: inline-block;
-   min-width: 100px;
-   padding: 0px 5px;
-   margin: 0;
-   margin-left: 2%;
+  display: inline-block;
+  min-width: 100px;
+  padding: 0px 5px;
+  margin: 0;
+  margin-left: 2%;
 `;
--- src/App/Form/styled.js
+++ src/App/Form/styled.js
@@ -1,25 +1,24 @@
 import styled, { css } from "styled-components";
 
 export const Input = styled.input`
-   background: ${({ theme }) => theme.color.white};
-   color: ${({ theme }) => theme.color.black};
-   border: 1px solid ${({ theme }) => theme.color.Silver};
-   padding: 5px 10px;
-   min-width: 230px;
-   width: 70%;
-   border-radius: 5px;
-   font-size: 18px;
-   margin-bottom: 10px;
+  background: ${({ theme }) => theme.color.white};
+  color: ${({ theme }) => theme.color.black};
+  border: 1px solid ${({ theme }) => theme.color.Silver};
+  padding: 5px 10px;
+  width: 70%;
+  border-radius: 5px;
+  font-size: 18px;
+  margin-bottom: 10px;
 
-   ${({ rate }) => rate && css`
-   text-align: right;
-   `}
+  ${({ rate }) => rate && css`
+    text-align: right;
+  `}
 
-   @media(max-width: ${({ theme }) => theme.breakpoint.mobileMin}px) {
-      width: 100%;
-   }
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMin}px) {
+    width: 100%;
+  }
 `;
 
 export const Select = styled(Input)`
-padding-left: 50px;
+  padding-left: 50px;
 `;
