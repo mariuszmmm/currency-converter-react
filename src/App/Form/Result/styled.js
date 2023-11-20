bf2ca0632abf983fc430315afcdabb8b5bc5ddd9
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-11-20 16:17:36 +0100

    changed tab display size

--- src/App/Form/Result/styled.js
+++ src/App/Form/Result/styled.js
@@ -1,28 +1,28 @@
 import styled, { css } from "styled-components";
 
 export const Container = styled.div`
-   margin: 10px 0;
-   width: auto;
-   font-size: 30px;
-   text-align: center;
-   font-weight: 700;
-   text-shadow: 1px 1px 10px ${({ theme }) => theme.color.doveGray};
-   padding: 0;
-   color: ${({ theme }) => theme.color.red};
+  margin: 10px 0;
+  width: auto;
+  font-size: 30px;
+  text-align: center;
+  font-weight: 700;
+  text-shadow: 1px 1px 10px ${({ theme }) => theme.color.doveGray};
+  padding: 0;
+  color: ${({ theme }) => theme.color.red};
 
-   ${({ outdated }) => outdated && css`
-      transition: 1s;
-      opacity: 0;
-   `}
+  ${({ outdated }) => outdated && css`
+    transition: 1s;
+    opacity: 0;
+  `}
 `;
 
 export const AmountInfo = styled.p`
-   margin: 10px 0;
-   font-size: 20px;
-   color: ${({ theme }) => theme.color.black};
+  margin: 10px 0;
+  font-size: 20px;
+  color: ${({ theme }) => theme.color.black};
 `;
 
 export const ResultInfo = styled(AmountInfo)`
-   font-size: 35px;
-   color: ${({ theme }) => theme.color.red};
+  font-size: 35px;
+  color: ${({ theme }) => theme.color.red};
 `;
